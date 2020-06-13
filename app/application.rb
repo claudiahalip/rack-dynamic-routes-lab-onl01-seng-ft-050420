8fb19b8024202fce51abc9cd5e8acef82bc7cfb1
claudia v <claudia4ro@yahoo.com>
2020-06-13 03:25:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,17 +9,21 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if @@items.include?(item_name)
-        resp.write item_name.price
-      else 
-        resp.write "Item not found"
-        resp.status = 400
+      resp.write item_price
       end
     else 
       resp.write "Route not found"       
       resp.status = 404
     end 
     
+    def item_price(item_name)
+      if @@items.include?(item_name)
+        return item_name.price
+      else 
+        return "Item not found"
+        resp.status = 400
+      
+    end 
      
     
   end 
